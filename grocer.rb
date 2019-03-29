da14712db0b703d3899cc0ef924c878ef4cb7b8b
Joe Tustin <j.tustin@gmail.com>
2019-03-29 17:48:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -182,6 +182,7 @@ def apply_clearance(cart)
   cart.each do |item, item_hash|
     if item_hash[:clearance] == true
       item_hash[:price] = '%.2f' % (item_hash[:price] * 0.8)
+                          ((item_hash[:price] * 0.8)*100).round / 100
 
 
 
