a6c58bfed930a06562fa1f6d1a4eb06ca8dd868b
Joe Tustin <j.tustin@gmail.com>
2019-03-29 17:08:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -108,7 +108,7 @@ end
 #man that was a doozy, applied what I learned from other labs to create and modify the hashes.
 #a space in the w/coupon sent me on a goose chase for a while
 #interesting to think about how to structure and explore data structures, and what to plan for
-# created an array which took the couopon item name and stored in array, once a coupon was removed that value was deleted. 
+# created an array which took the couopon item name and stored in array, once a coupon was removed that value was deleted.
 # I think there is an easier way to do this but this does work
 require 'pry'
 def apply_coupons(cart, coupons)
